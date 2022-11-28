294595c21a41661a94942025c42c0885f843eddf
Akitsuyoshi <ka090824@gmail.com>
2022-11-28 21:03:49 +0900

    Add basic rabbitmq publish and consume method

--- applications/basic_server/app.js
+++ applications/basic_server/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const promMid = require('express-prometheus-middleware');
+const amqplib = require('amqplib')
+const amqpUrl = process.env.AMPQ_URL || 'amqp://rabbitmq:5672'
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -13,6 +15,18 @@ app.use(promMid({
     responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
 }))
 
+const connect = async (connection, channel, amqpObj, msg) => {
+    connection = await amqplib.connect(amqpUrl, 'heartbeat=60')
+    channel = await connection.createChannel()
+    const { exchange, queue, routingKey } = amqpObj
+    await channel.assertExchange(exchange, 'direct', { durable: true })
+    await channel.assertQueue(queue, { durable: true })
+    await channel.bindQueue(queue, exchange, routingKey)
+    await channel.publish(exchange, routingKey, Buffer.from(JSON.stringify(msg)))
+    console.log('Message published')
+}
+
+
 app.get('/', async (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'))
 })
@@ -23,6 +37,23 @@ app.get('/health', (req, res) => {
 
 app.post('/echo_user_input', (req, res) => {
     res.send(req.body.user_input)
+})
+
+app.post('/rabbit_publish', async (req, res) => {
+    let connection
+    let channel
+    try {
+        const amqpObj = {
+            exchange: 'sample_exchange',
+            queue: 'sample_queue',
+            routingKey: '1',
+        }
+        const msg = { 'id': 'sample', 'body': 'first_msg' }
+        await connect(connection, channel, amqpObj, msg)
+        res.send(msg)
+    } catch (err) {
+        console.log('error in publishing message: ', err)
+    }
 })
 
 module.exports = app
--- applications/data_analyzer_server/index.js
+++ applications/data_analyzer_server/index.js
@@ -1,7 +1,37 @@
 const port = process.env.PORT || 3001
 const app = require('./app')
+const amqplib = require('amqplib')
+const amqpUrl = process.env.AMQP_URL || 'amqp://rabbitmq:5672'
 
-app.listen(port, () => {
+const processMessage = async (msg) => {
+    console.log('Received message: ' + msg.content.toString())
+    // Some async function follows here
+}
+
+const connect = async (mseconds) => {
+    const connection = await amqplib.connect(amqpUrl, 'heartbeat=60')
+    const channel = await connection.createChannel()
+    channel.prefetch(10)
+    process.once('SIGINT', async () => {
+        await channel.close()
+        await connection.close()
+        process.exit(0)
+    })
+
+    const queue = 'sample_queue'
+    await channel.assertQueue(queue, { durable: true })
+    await channel.consume(queue, async (msg) => {
+        await processMessage(msg)
+        await channel.ack(msg)
+    }, {
+        noAck: false,
+        consumerTag: 'sample_consumer'
+    })
+    console.log('Waiting for the message')
+}
+connect()
+
+app.listen(port, async () => {
     console.log(`Example app listening on port ${port}`)
 })
 
